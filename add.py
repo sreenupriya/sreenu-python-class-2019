a56ab1324fd036f029fbe50f477a53e06ae86cd0
AJ <ajmohan26@gmail.com>
2019-02-23 12:47:58 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,8 @@
+def display():
+    print(__name__)
+
+
+def addition(x,y):
+    z=x+y
+    return z
+
---
+++ calc.py
@@ -0,0 +1,11 @@
+import add
+
+
+# num1=int(input("enter number 1:"))
+# num2=int(input("enter number 2:"))
+# result=add.addition(num1,num2)
+# print(result)
+
+
+print(__name__)
+add.display()
